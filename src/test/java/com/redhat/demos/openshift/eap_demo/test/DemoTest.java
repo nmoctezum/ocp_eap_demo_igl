0c4263ca75d91626949416f69a1e1bb85171c46c
Ramón Moctezuma <nmoctezum@gmail.com>
2020-03-12 08:46:28 -0600

    inicial

---
+++ src/test/java/com/redhat/demos/openshift/eap_demo/test/DemoTest.java
@@ -0,0 +1,14 @@
+package com.redhat.demos.openshift.eap_demo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test() {
+		System.out.println("done..");
+	}
+
+}
